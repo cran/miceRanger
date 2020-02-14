6fed8e04a52ee273c65919e06183da91da6a5fba
Sam Wilson <samwilson303@gmail.com>
2020-02-14 11:40:02 +0000

    version 1.3.1

--- inst/doc/diagnosticPlotting.R
+++ inst/doc/diagnosticPlotting.R
@@ -1,34 +1,43 @@
-## ----amputeData,message=FALSE-------------------------------------------------
-require(miceRanger)
-set.seed(1)
+params <-
+list(EVAL = TRUE)
 
-# Load data
-data(iris)
-
-# Ampute the data. iris contains no missing values by default.
-ampIris <- amputeData(iris,perc=0.25)
-
-miceObj <- miceRanger(
-    ampIris
-  , m=6
-  , verbose=FALSE
+## ---- SETTINGS-knitr, include=FALSE-------------------------------------------
+stopifnot(require(knitr))
+opts_chunk$set(
+  eval = if (isTRUE(exists("params"))) params$EVAL else FALSE
 )
 
+## ----amputeData,message=FALSE-------------------------------------------------
+#  require(miceRanger)
+#  set.seed(1)
+#  
+#  # Load data
+#  data(iris)
+#  
+#  # Ampute the data. iris contains no missing values by default.
+#  ampIris <- amputeData(iris,perc=0.25)
+#  
+#  miceObj <- miceRanger(
+#      ampIris
+#    , m=6
+#    , verbose=FALSE
+#  )
+
 ## ----plotDistributions--------------------------------------------------------
-plotDistributions(miceObj,vars='allNumeric')
+#  plotDistributions(miceObj,vars='allNumeric')
 
 ## ----plotCorrelations---------------------------------------------------------
-plotCorrelations(miceObj,vars='allNumeric')
+#  plotCorrelations(miceObj,vars='allNumeric')
 
 ## ----plotVarConvergence-------------------------------------------------------
-plotVarConvergence(miceObj,vars='allNumeric')
+#  plotVarConvergence(miceObj,vars='allNumeric')
 
 ## ----plotModelError-----------------------------------------------------------
-plotModelError(miceObj,vars='allNumeric')
+#  plotModelError(miceObj,vars='allNumeric')
 
 ## ----plotVarImportance--------------------------------------------------------
-plotVarImportance(miceObj)
+#  plotVarImportance(miceObj)
 
 ## ----plotImputationVariance, fig.height = 4, fig.width = 8--------------------
-plotImputationVariance(miceObj,ncol=2,widths=c(5,3))
+#  plotImputationVariance(miceObj,ncol=2,widths=c(5,3))
 
